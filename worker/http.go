d64f1fcb0eac190f54ead0312e3f9c58835dd914
Tang, Tom <Tom.Tang@news.com.au>
2016-06-06 07:21:25 +1000

    ms

--- worker/http.go
+++ worker/http.go
@@ -6,26 +6,47 @@ import (
   "log"
   "net/http"
   "io"
+  "strconv"
   //"godown/writer"
 )
 
 const SIZE_WRITEBLOCK = 1024 * 100 * 4 
 const RNAGE_BYTES_FMT = "bytes=%d-%d"
+const HTTP_TIMEOUT_SECS = 30
 
 type PartWork struct{
-  start int
-  length int
+  url string
+  cookie map[string] string
+  start uint64
+  length uint64
   buf []byte
 }
 
+func new_http_client() *http.Client {
+  netTransport := &http.Transport{
+    Dial: (&net.Dialer{
+      Timeout: 5 * time.Second,
+    }).Dial,
+    TLSHandshakeTimeout: 5 * time.Second,
+  }
+  return &http.Client{
+    Timeout: time.Second * HTTP_TIMEOUT_SECS,
+    Transport: netTransport,
+  }
+}
 
 type HttpWorker struct {
-  url string
-  cookie map[string] string
   ch_partwork_get chan PartWork
   ch_partwork_store chan PartWork
   client *http.Client
   buf []byte
+}
+
+func new_worker(url string, client *http.Client){
+  return &HttpWorker{
+    url: url,
+    client: client
+  }
 }
 
 func (self *HttpWorker) init(){
@@ -67,15 +88,16 @@ func (self *HttpWorker) do(pwork *PartWork) (int, error) {
 
   defer resp.Body.Close()
   reader := bufio.NewReader(resp.Body)
-  //log.Printf("self.buf", self.buf)
+
   n_read, err := reader.Read(self.buf)
   if err != io.EOF && err != nil {
     log.Printf("Failed in reading from resp: %v", err)
     return -1, err
   }
 
-  if n_read != pwork.length {
-    return -1, fmt.Errorf("n_read: %v != PartWork.length: %v (requested)",
+  if uint64(n_read) != pwork.length {
+    return -1, fmt.Errorf(
+        "n_read: %v != PartWork.length: %v (requested)",
         n_read, pwork.length)
   }
 
@@ -84,10 +106,96 @@ func (self *HttpWorker) do(pwork *PartWork) (int, error) {
   return n_read, nil
 }
 
-type HttpManager struct {
+type HttpDownloader struct {
+  worker_count int
   workers [] *HttpWorker
-
+  cookie map[string] string
+  ch_work chan string
   ch_partwork_get chan PartWork
   ch_partwork_store chan PartWork
+  ch_done chan bool
+  ch_done_worker chan bool
+  client *http.Client
 }
 
+func NewHttpDownloader(worker_count int, ch_work chan, ch_done chan) * {
+  hdownloaer = & HttpDownloader{
+    worker_count: worker_count,
+    ch_work: ch_work,
+    ch_done: ch_done,
+    client: new_http_client()
+  }
+}
+
+func (self *NewHttpDownloader) Init() {
+  for (var i = 0; i < self.worker_count; i ++ ){
+
+  }
+}
+
+
+func (self *HttpDownloader) get_size(url string) (uint64, error) {
+  req, err := http.NewRequest("HEAD", url, nil)
+
+  if err != nil {
+    log.Printf("Failed to create http request to:%v, %v", url, err)
+    return 0, err
+  }
+
+  for k, v := range self.cookie {
+    req.Header.Set(k, v)
+  }
+
+  resp, err := self.client.Do(req)
+
+  if err != nil {
+    return 0, err
+  }
+
+  defer resp.Body.Close()
+
+  if val, ok := resp.Header["Content-Length"]; ok {
+    size, err := strconv.ParseUint(val[0], 10, 64)
+
+    if (err == nil ) {
+      return size, nil
+    }
+  }
+  
+  return 0, err
+}
+
+
+func (self *HttpDownloader) GetSize(url string) (uint64, error) {
+  req, err := http.NewRequest("HEAD", url, nil)
+
+  if err != nil {
+    log.Printf("Failed to create http request to:%v, %v", url, err)
+    return 0, err
+  }
+
+  for k, v := range self.cookie {
+    req.Header.Set(k, v)
+  }
+
+  resp, err := self.client.Do(req)
+
+  if err != nil {
+    return 0, err
+  }
+
+  defer resp.Body.Close()
+
+  if val, ok := resp.Header["Content-Length"]; ok {
+    size, err := strconv.ParseUint(val[0], 10, 64)
+
+    if (err == nil ) {
+      return size, nil
+    }
+  }
+  
+  return 0, err
+}
+
+func (self *HttpDownloader) Fetch(url string, path string) (uint64, error) {
+}
--- worker/http_test.go
+++ worker/http_test.go
@@ -1,6 +1,9 @@
 package worker
 
-import "testing"
+import (
+  "testing"
+  "net/http"
+)
 
 func TestWorkerDo(t *testing.T) {
   url := "http://localhost:8080/test1"
@@ -13,6 +16,19 @@ func TestWorkerDo(t *testing.T) {
   n_read, err := http_worker.do(&pw)
   if n_read != 100 {
     t.Error("n_read is not equal to what requested")
+  }
+  if err != nil {
+    t.Error("err is not nil")
+  }
+}
+
+func TestHttpdownGetsize(t *testing.T) {
+  url := "http://localhost:8080/test1"
+  hd := HttpDownloader {client: &http.Client{}}
+
+  n_size, err := hd.GetSize(url)
+  if n_size != 4096 {
+    t.Error("n_size is not equal to what requested")
   }
   if err != nil {
     t.Error("err is not nil")
