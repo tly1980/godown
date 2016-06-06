db2a5d2abbd12be0706b1aa3c0f0c25cac7f15de
Tang, Tom <Tom.Tang@news.com.au>
2016-06-07 07:11:02 +1000

    msc

--- worker/http.go
+++ worker/http.go
@@ -7,19 +7,24 @@ import (
   "net/http"
   "io"
   "strconv"
+  "net"
+  "time"
   //"godown/writer"
 )
 
 const SIZE_WRITEBLOCK = 1024 * 100 * 4 
 const RNAGE_BYTES_FMT = "bytes=%d-%d"
 const HTTP_TIMEOUT_SECS = 30
+const WORKER_MAX_RETIREX = 3
 
 type PartWork struct{
   url string
   cookie map[string] string
   start uint64
   length uint64
+  n_read uint64
   buf []byte
+  try_count int
 }
 
 func new_http_client() *http.Client {
@@ -36,43 +41,53 @@ func new_http_client() *http.Client {
 }
 
 type HttpWorker struct {
-  ch_partwork_get chan PartWork
-  ch_partwork_store chan PartWork
+  ch_in chan PartWork
+  ch_out chan PartWork
   client *http.Client
   buf []byte
 }
 
-func new_worker(url string, client *http.Client){
+func new_worker(
+  ch_in chan PartWork, ch_out chan PartWork, client *http.Client) *HttpWorker {
+
   return &HttpWorker{
-    url: url,
-    client: client
+    ch_in: ch_in,
+    ch_out: ch_out,
+    client: client,
+    buf: make([]byte, SIZE_WRITEBLOCK),
   }
 }
 
-func (self *HttpWorker) init(){
-  self.client = &http.Client{}
-  self.buf = make([]byte, SIZE_WRITEBLOCK);
-}
 
 func (self *HttpWorker) run(){
   //Initialize the REQUEST
 
-  for w := range self.ch_partwork_get {
-    self.do(&w)
-    w.buf = self.buf
-    self.ch_partwork_store <- w
+  for w := range self.ch_in {
+    for i := 0; i < WORKER_MAX_RETIREX; i++ {
+      w.try_count++
+      n_read, err := self.do(&w)
+      if (err == nil ){
+        w.buf = self.buf
+        w.n_read = uint64(n_read)
+        break
+      }else{
+        log.Printf("err: %s", err)
+      }
+    }
+
+    self.ch_out <- w
   }
 }
 
 func (self *HttpWorker) do(pwork *PartWork) (int, error) {
-  req, err := http.NewRequest("GET", self.url, nil)
+  req, err := http.NewRequest("GET", pwork.url, nil)
 
   if err != nil {
-    log.Printf("Failed to create http request to:%v, %v", self.url, err)
+    log.Printf("Failed to create http request to:%v, %v", pwork.url, err)
     return -1, err
   }
 
-  for k, v := range self.cookie {
+  for k, v := range pwork.cookie {
     req.Header.Set(k, v)
   }
 
@@ -101,8 +116,6 @@ func (self *HttpWorker) do(pwork *PartWork) (int, error) {
         n_read, pwork.length)
   }
 
-  log.Printf("%v", string(self.buf[0:n_read]))
-
   return n_read, nil
 }
 
@@ -118,20 +131,23 @@ type HttpDownloader struct {
   client *http.Client
 }
 
-func NewHttpDownloader(worker_count int, ch_work chan, ch_done chan) * {
-  hdownloaer = & HttpDownloader{
+func NewHttpDownloader(
+  worker_count int, ch_work chan string, ch_done chan bool) *HttpDownloader {
+  hdownloaer := & HttpDownloader{
     worker_count: worker_count,
     ch_work: ch_work,
     ch_done: ch_done,
-    client: new_http_client()
+    client: new_http_client(),
   }
+
+  return hdownloaer
 }
 
-func (self *NewHttpDownloader) Init() {
-  for (var i = 0; i < self.worker_count; i ++ ){
+// func (self *NewHttpDownloader) Init() {
+//   for (var i = 0; i < self.worker_count; i ++ ){
 
-  }
-}
+//   }
+// }
 
 
 func (self *HttpDownloader) get_size(url string) (uint64, error) {
@@ -197,5 +213,5 @@ func (self *HttpDownloader) GetSize(url string) (uint64, error) {
   return 0, err
 }
 
-func (self *HttpDownloader) Fetch(url string, path string) (uint64, error) {
-}
+// func (self *HttpDownloader) Fetch(url string, path string) (uint64, error) {
+// }
--- worker/http_test.go
+++ worker/http_test.go
@@ -1,36 +1,86 @@
 package worker
 
 import (
+  "bufio"
+  "fmt"
+  "log"
+  "os"
   "testing"
-  "net/http"
+  "path"
+  "path/filepath"
+
+  "github.com/stretchr/testify/assert"
 )
 
+const TEST_DATA_FOLDER = "../test"
+
+func read(path string, start int64, length int64) []byte{
+  file, err := os.Open(path)
+  if (err != nil ) {
+    log.Fatal(err)
+  }
+
+  buf := make([]byte, length)
+  defer file.Close()
+  file.Seek(start, os.SEEK_SET)
+
+  reader := bufio.NewReader(file)
+
+  nread, err := reader.Read(buf)
+
+  if int64(nread) != length {
+    log.Fatal("asd")
+  }
+
+  return buf
+}
+
 func TestWorkerDo(t *testing.T) {
-  url := "http://localhost:8080/test1"
-  http_worker := HttpWorker{
-    url: url,
-  }
+  ch_in := make(chan PartWork)
+  ch_out := make(chan PartWork)
 
-  pw := PartWork{start: 0, length: 100}
-  http_worker.init()
-  n_read, err := http_worker.do(&pw)
-  if n_read != 100 {
-    t.Error("n_read is not equal to what requested")
-  }
+  base_url := "http://localhost:8080/"
+  base_path, err := filepath.Abs(path.Join("..", "test"))
   if err != nil {
-    t.Error("err is not nil")
+    log.Fatal(err)
   }
+  fname := "test1"
+
+  url := base_url + fname
+  fpath := path.Join(base_path, fname)
+
+  fmt.Println("url: ", url)
+  client := new_http_client()
+  http_worker := new_worker(ch_in, ch_out, client)
+
+  pw := PartWork{
+    start: 0, length: 100, url: url, 
+  }
+  go http_worker.run()
+
+  ch_in <- pw
+  pw2 := <-ch_out
+
+  defer close(ch_in)
+  defer close(ch_out)
+
+  buf := read(fpath, 0, 100)
+
+  //fmt.Printf("%v, %v\n", pw2.try_count, pw2.buf[:pw2.length])
+
+  assert.Equal(t, pw2.try_count, 1,  "they should be equal")
+  assert.Equal(t, buf, pw2.buf[:pw2.length],  "they should be equal")
 }
 
-func TestHttpdownGetsize(t *testing.T) {
-  url := "http://localhost:8080/test1"
-  hd := HttpDownloader {client: &http.Client{}}
+// func TestHttpdownGetsize(t *testing.T) {
+//   url := "http://localhost:8080/test1"
+//   hd := HttpDownloader {client: &http.Client{}}
 
-  n_size, err := hd.GetSize(url)
-  if n_size != 4096 {
-    t.Error("n_size is not equal to what requested")
-  }
-  if err != nil {
-    t.Error("err is not nil")
-  }
-}
+//   n_size, err := hd.GetSize(url)
+//   if n_size != 4096 {
+//     t.Error("n_size is not equal to what requested")
+//   }
+//   if err != nil {
+//     t.Error("err is not nil")
+//   }
+// }
